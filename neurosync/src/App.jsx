141686a0ae4e7f51e9166c3cd38a3a4ed1d6c290
Olamirinde <adekunleamosolarinde@gmail.com>
2025-06-20 19:45:55 +0100

    Project Setup

---
+++ neurosync/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <p className='text-3xl text-green-500'>Hello world</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
