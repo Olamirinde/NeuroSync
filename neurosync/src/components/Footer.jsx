35dc7a3b1fc3dca1c2e741d93155bc43eb99d67f
Olamirinde <adekunleamosolarinde@gmail.com>
2025-06-22 01:04:07 +0100

    Added favicon logo

--- neurosync/src/components/Footer.jsx
+++ neurosync/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { resourcesLinks, platformLinks, communityLinks } from "../constants"
 
 const Footer = () => {
   return (
-    <footer className="mt-20 border-t mx-6 py-20 border-neutral-700">
+    <footer className="mt-20 border-t mx-10 py-20 border-neutral-700">
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
             <div>
                 <h3 className="text-md font-semibold mb-4">Resources</h3>
